fd4c2a29c97e753ce7a4542a8f7569d985e537a0
Juan García Montero <jgmen20@gmail.com>
2022-10-10 21:49:30 +0200

    Security

--- spring-segurity/spring-segurity/src/main/java/com/example/springsegurity/WebSecurityConfig.java
+++ spring-segurity/spring-segurity/src/main/java/com/example/springsegurity/WebSecurityConfig.java
@@ -2,22 +2,74 @@ package com.example.springsegurity;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends EnableWebSecurity {
-    public WebSecurityConfig() {
+public class WebSecurityConfig {
 
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http.authorizeRequests().antMatchers("/public/**").permitAll().anyRequest()
-                    .hasRole("USER").and()
-                    // Possibly more configuration ...
-                    .formLogin() // enable form based log in
-                    // set permitAll for all URLs associated with Form Login
-                    .permitAll();
-            return http.build();
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+                // Spring Security should completely ignore URLs starting with /resources/
+                .antMatchers("/resources/**");
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/hola").permitAll()
+                //.antMatchers("/bootstrap").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                //.authenticated()
+                .and()
+
+                // Possibly more configuration ...
+                .formLogin()
+                .and()
+                // enable form based log in
+                // set permitAll for all URLs associated with Form Login
+                //.permitAll();
+                .httpBasic();
+
+        return http.build();
+    }
+
+
+    // Permite cambiar Firewall por defecto de Spring
+    // Cambiando para permitir en la URL BackSlash "/" y Semicolon ";"
+    /*@Bean
+    public HttpFirewall looseHttpFirewall(){
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowBackSlash(true);
+        firewall.setAllowSemicolon(true);
+
+        return firewall;
+    }*/
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =  User.builder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN", "USER")
+                .build();
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+
 }
